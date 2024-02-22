0b234dcd3944dc1a5af9bccdb2ffbe1a4e3ea421
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-02-22 08:29:34 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+
+  return (
+    <>
+      <View style={{}}>
+        <Text>Rick and Morty App</Text>
+      </View>
+    </>
+  )
+};
+export default App;
